d91661a16152592d3312d2de5bfa127cd8fa17be
Kenneth Dan Weinberg Waks <kenneth@moveo.co.il>
2024-08-04 10:16:52 +0300

    v1.0.2

--- src/module/Deposits.jsx
+++ src/module/Deposits.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Deposits() {
 	return (
 		<div>
-			This is deposits module
+			This is deposits module - v2
 			<br />
 			<br />
 			<Button backgroundColor="blue" label="Button v1.0.5" />
